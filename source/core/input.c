1f34444152dd0f0d6c0047bf4509e972b3d73d8e
PierreLgol <plgol.perso@gmail.com>
2024-05-25 15:02:18 +0200

    added basic input-loop and signal handling

--- source/core/input.c
+++ source/core/input.c
@@ -20,26 +20,42 @@ t_shell_input *shell_input_create(t_allocator *allocator)
 	self = allocator->create(allocator, sizeof(*self));
 	assert(self != NULL);
 	self->allocator = allocator;
+	self->line = NULL;
 	return (self);
 }
 
-void shell_input_init(t_allocator *allocator, t_shell_input *self)
+void shell_input_init(t_allocator *allocator, t_shell_input *self, char *prompt)
 {
+	char *temp;
+
 	assert(allocator != NULL);
 	assert(self != NULL);
+	assert(prompt != NULL);
+	temp = readline(prompt);
+	if (!temp)
+		self->line = NULL;
+	else
+	{
+		self->line = allocator->dupz(allocator, temp, string_length(temp));
+		free(temp);
+	}
 }
 
 void shell_input_deinit(t_allocator *allocator, t_shell_input *self)
 {
 	assert(allocator != NULL);
 	assert(self != NULL);
+	if (self->line)
+	{
+		allocator->destroy(allocator, self->line);
+		self->line = NULL;
+	}
 }
 
 t_shell_input *shell_input_destroy(t_allocator *allocator, t_shell_input *self)
 {
 	assert(self != NULL);
 	assert(allocator != NULL);
-
 	allocator->destroy(allocator, self);
 	return (NULL);
 }
--- source/core/prompt.c
+++ source/core/prompt.c
@@ -20,18 +20,36 @@ t_shell_prompt *shell_prompt_create(t_allocator *allocator)
 	self = allocator->create(allocator, sizeof(*self));
 	assert(self != NULL);
 	self->allocator = allocator;
+	self->prompt = NULL;
 	return (self);
 }
 
-void shell_prompt_init(t_allocator *allocator, t_shell_prompt *self)
+void shell_prompt_init(t_allocator *allocator, t_shell_prompt *self, char *pwd)
 {
+	char *temp1;
+	char *temp2;
+
 	assert(allocator != NULL);
 	assert(self != NULL);
+	assert(pwd != NULL);
+	temp1 = string_join_sequence(allocator, YELLOW, pwd);
+	assert(temp1 != NULL);
+	temp2 = string_join_sequence(allocator, temp1, " |> ");
+	assert(temp2 != NULL);
+	self->prompt = string_join_sequence(allocator, temp2, RESET);
+	assert(self->prompt != NULL);
+	allocator->destroy(allocator, temp1);
+	allocator->destroy(allocator, temp2);
 }
 void shell_prompt_deinit(t_allocator *allocator, t_shell_prompt *self)
 {
 	assert(allocator != NULL);
 	assert(self != NULL);
+	if (self->prompt)
+	{
+		allocator->destroy(allocator, self->prompt);
+		self->prompt = NULL;
+	}
 }
 
 t_shell_prompt *shell_prompt_destroy(t_allocator *allocator, t_shell_prompt *self)
